e3bff27774b4f928c8e8ce672714d0986b792fbf
tobacoguys <dungchelsea1723@gmail.com>
2024-12-18 00:56:33 +0700

    config jwt

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,6 +1,6 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
-export class SignUpDto {
+export class SignupDto {
   @IsNotEmpty()
   @IsString()
   username: string;
