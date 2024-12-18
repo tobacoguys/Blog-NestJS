7dcaa26062c9724a874e51ecc8279693b212f35e
Nguyen Nam Duong <nguyennamduong1212@gmail.com>
2024-12-18 11:40:21 +0700

    commit

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 @Entity('user')
 class User {
   @PrimaryGeneratedColumn()
-  id: number;
+  id: string;
 
   @Column()
   username: string;
