237ef90ac959087f2f4325aebbe17774c5bec2bd
tobacoguys <dungchelsea1723@gmail.com>
2024-12-18 22:21:08 +0700

    done API update profile user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Patch, Request } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { Private } from '../auth/decorator/private.decorator';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Patch('/profile')
+  @Private()
+  async updateProfile(@Request() req, @Body() updateUserDto: UpdateUserDto) {
+    const userId = req.user.id;
+    const updatedUser = await this.userService.updateProfile(
+      userId,
+      updateUserDto,
+    );
+    return { message: 'Profile updated successfully', data: updatedUser };
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,3 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import User from './user.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
+
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async updateProfile(
+    userId: string,
+    updateUserDto: UpdateUserDto,
+  ): Promise<User> {
+    const user = await this.userRepository.findOne({ where: { id: userId } });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const { username, bio, birthday } = updateUserDto;
+    if (username) user.username = username;
+    if (bio) user.bio = bio;
+    if (birthday) user.birthday = birthday;
+
+    await this.userRepository.save(user);
+    return user;
+  }
+}
