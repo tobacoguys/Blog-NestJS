1dac5ffe0c889549c809294a01b6ace5a9a91077
tobacoguys <dungchelsea1723@gmail.com>
2024-12-19 08:52:51 +0700

    admin guard

---
+++ src/auth/guard/admin.guard.ts
@@ -0,0 +1,18 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+export class AdminGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    if (!user || user.isAdmin) {
+      throw new UnauthorizedException('Access denied. Admin only.');
+    }
+
+    return true;
+  }
+}
