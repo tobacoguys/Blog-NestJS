76e9c346e7e6564f05a541b4f425c72ebc02602b
tobacoguys <dungchelsea1723@gmail.com>
2025-01-10 11:59:32 +0700

    API create report post

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -2,11 +2,23 @@ import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { ReportService } from './report.service';
 import { CreateReportDto } from './dto/create-report.dto';
 import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('report')
 export class ReportController {
   constructor(private readonly reportService: ReportService) {}
 
+  @ApiTags('Report')
+  @ApiBearerAuth('token')
+  @ApiOperation({ 
+    summary: 'Create a report',
+    description: 'Allows a logged-in user to report a post'  
+  }) 
+  @ApiResponse({
+    status: 201,
+    description: 'Report created successfully',
+    type: CreateReportDto,
+  })
   @Post('/create')
   @UseGuards(JwtAuthGuard)
   async createReport(@Body() createReportDto: CreateReportDto, @Request() req) {
