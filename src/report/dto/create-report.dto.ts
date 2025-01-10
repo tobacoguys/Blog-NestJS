ced6b9a2a14624a6be5419c3bca89925536ee9ab
tobacoguys <dungchelsea1723@gmail.com>
2025-01-10 11:45:54 +0700

    create report Dto

---
+++ src/report/dto/create-report.dto.ts
@@ -0,0 +1,10 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateReportDto {
+  @IsNotEmpty()
+  @IsString()
+  reason: string;
+
+  @IsNotEmpty()
+  postId: number;
+}
