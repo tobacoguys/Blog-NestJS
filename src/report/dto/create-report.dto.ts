1d3197f3955c3f6d4d79f5234aa630f08f24dc37
tobacoguys <dungchelsea1723@gmail.com>
2025-01-10 12:01:35 +0700

    add swagger

--- src/report/dto/create-report.dto.ts
+++ src/report/dto/create-report.dto.ts
@@ -1,10 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateReportDto {
+  @ApiProperty({
+    description: 'The reason for reporting the post.',
+    example: 'This post contains inappropriate content',
+  })
   @IsNotEmpty()
   @IsString()
   reason: string;
 
+  @ApiProperty({
+    description: 'The ID of the post being reported.',
+    example: '1',
+  })
   @IsNotEmpty()
   postId: string;
 }
